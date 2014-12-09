714f108730010e822792ae0c87e453b08b44c29b
RColeman <s00157859@mail.itsligo.ie>
2014-12-09 15:09:50 +0000

    CA2 Assignment

---
+++ CA2/Controllers/HomeController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Diagnostics;
+using System.Web.Mvc;
+using System.Data.Entity;
+using CA2.Models;
+
+namespace CA2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+        MovieDb db = new MovieDb();   //Datbase Connection
+        //
+        // GET: /Home/
+        public ActionResult Index(string sortOrder)
+        {
+
+            ViewBag.PageTitle = "List of Movies";
+            ViewBag.numOrder = String.IsNullOrEmpty(sortOrder)?"descNumber":"";
+            ViewBag.dateOrder= sortOrder == "ascDate" ? "descDate" : "ascDate";
+
+            IQueryable<Movie> Movies = db.Movies;
+            switch (sortOrder)
+            {
+                case "descDate":
+                    Movies = Movies.OrderByDescending(c => c.StartDate).Include("Actors");
+                    break;
+                case "descNumber":
+                    Movies = Movies.OrderByDescending(c => c.Actors.Count).Include("Actors");
+                    break;
+                case "ascDate":
+                    Movies = Movies.OrderBy(c => c.StartDate).Include(c => c.Actors);
+                    break;
+                default:
+                    Movies = Movies.OrderBy(c => c.Actors.Count).Include("Actors");
+                    break;
+            }
+            return View(Movies.ToList());
+        }  
+        //
+        // GET: /Home/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id==null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var q = db.Movies.Find(id); 
+            if (q == null)  // find record?
+            {
+                Debug.WriteLine("Record not found");
+                ViewBag.PageTitle = String.Format("Sorry, record {0} not found.", id);
+                //return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+            }
+            else ViewBag.PageTitle = "Details of " + q.Title + " (" + ((q.Actors.Count==0)?"None":q.Actors.Count.ToString()) + ')';
+            return View(q);
+        }
+
+        //
+        // GET: /Home/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(db.Movies, "MovieId", "Title");
+            return View();
+        }
+
+        //
+        // POST: /StoreManager/Create
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(movie);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "Title", movie.MovieId);
+            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "Name", movie.Actors);
+            return View(movie);
+        }
+
+        //
+        // GET: /Home/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "Title", movie.MovieId);
+            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "Title", movie.Actors);
+            return View(movie);
+        }
+
+        
+         //POST: /StoreManager/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MovieId = new SelectList(db.Movies, "MovieId", "Name", movie.MovieId);
+            ViewBag.ActorId = new SelectList(db.Actors, "ActorId", "Name", movie.Actors);
+            return View(movie);
+        }
+
+        
+        // GET: /Home/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            return View(movie);
+        }
+
+        //
+        // POST: /StoreManager/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Movie movie = db.Movies.Find(id);
+            db.Movies.Remove(movie);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+    }
---
+++ CA2/Models/Models.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CA2.Models
+{
+    class MovieDbInitialiser : DropCreateDatabaseAlways<MovieDb>
+    {
+        protected override void Seed(MovieDb context)
+        {
+            // seed the database with new Movies and Actors
+            Movie c = new Movie()
+            {
+                Title = "127 Hours",
+                StartDate = DateTime.Parse("4/6/2010")
+            };
+            context.Movies.Add(c);
+
+            Actor AlPacino = new Actor() { Name = "James Franco", Screename = "Aron Ralston" };
+            c.Actors = new List<Actor>();
+            c.Actors.Add(AlPacino);
+
+            context.Movies.Add(new Movie()
+            {
+                Title = "The Dark Knight",
+                StartDate = DateTime.Parse("12/1/2014"),
+                Actors = new List<Actor>()
+                {
+                    new Actor() {Name = "Christian Bale", Screename = "Bruce Wayne"},
+                    new Actor() {Name = "Maggie Gylenhaal", Screename = "Rachel Dawes"},
+                    new Actor() {Name = "Heath Ledger", Screename = "The Joker"}
+                }
+            });
+
+            context.Movies.Add(new Movie()
+            {
+                Title = "The Departed",
+                StartDate = DateTime.Parse("15/1/2012"),
+                Actors = new List<Actor>()
+                {
+                    new Actor() {Name = "Matt Damon", Screename = "Colin Sullivan"},
+                    new Actor() {Name = "Leonardo Di Caprio", Screename = "Billy Costigan"},
+                    new Actor() {Name = "Jack Nicolson", Screename = "Frank Costello"},
+                    new Actor() {Name = "Mark Wahlberg", Screename = "Sgt Dignam"}
+                }
+            });
+
+            context.Movies.Add(new Movie()
+            {
+                Title = "Wolf of Wall Street",
+                StartDate = DateTime.Parse("5/1/2009"),
+                Actors = new List<Actor>()
+                {
+                    new Actor() {Name = "Leonardo Di Caprio", Screename = "Jordan Belfort"},
+                    new Actor() {Name = "Jonah Hill", Screename = "Donnie Azoff"},
+                    new Actor() {Name = "Matthew McConaughey", Screename = "Mark Hanna"}
+                }
+            });
+
+            context.Movies.Add(new Movie()
+            {
+                Title = "Reign of Fire",
+                StartDate = DateTime.Parse("5/1/2009"),
+                Actors = new List<Actor>()
+                {
+                    new Actor() {Name = "Christian Bale", Screename = "Quinn Abercromby"},
+                    new Actor() {Name = "Matthew McConaughey", Screename = "Denton Van Zan"}
+                }
+            });
+
+        }
+    }
+
+    public class MovieDb : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public MovieDb()
+            : base("MovieDb")
+        { }
+    }
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        [Display(Name = "Movie")]
+        public string Title { get; set; }
+        [DisplayName("Opening Date"), DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "Actors")]
+        public virtual List<Actor> Actors { get; set; }
+    }
+
+    public class Actor
+    {
+        [Key]
+        public int ActorId { get; set; }
+        public string Name { get; set; }
+        public string Screename { get; set; }
+        public Movie Movie { get; set; }
+    }
+
+  
+
+}
